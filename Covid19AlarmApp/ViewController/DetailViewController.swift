cec5239564aafe8ea91b04ee74e656a047e9f10c
leegeonwoo <lgw9898@naver.com>
2021-08-04 14:24:32 +0900

    알림등록로직 개발완료

--- Covid19AlarmApp/ViewController/DetailViewController.swift
+++ Covid19AlarmApp/ViewController/DetailViewController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
+
+/*
+ UserDefault
+  - BellCheck : 벨이 켜졌는가 꺼졌는가?
+  - BellLocation : 켜졌다면 어느 지역을 선택했는가?
+ */
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var Header: UILabel! // 지역 위치
     @IBOutlet weak var NewsView: UIView!
+    @IBOutlet weak var naviRightItem: UIBarButtonItem!
     
     var detailViewModel = DetailViewModel.shared
     var Model = TableModel.shared // 싱글톤 객체 - 모델
@@ -18,7 +25,29 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         Header.text = "🇰🇷 지역 : \(Model.books[detailViewModel.row].gubun)"
+        naviImageSet()
     }
+    
+    @IBAction func naviRightItemClicked(_ sender: Any) {
+        
+        if UserDefaults.standard.integer(forKey: "BellCheck") == 1 && UserDefaults.standard.string(forKey: "BellLocation") == Model.books[detailViewModel.row].gubun {
+            UserDefaults.standard.set(0, forKey: "BellCheck")
+            self.naviRightItem.image = UIImage(systemName: "bell")
+            alert("매일 아침 알림을 헤제되었습니다.") {
+                UserDefaults.standard.set("nothing", forKey: "BellLocation") // 어떤 지역을 on했는가
+                self.naviRightItem.image = UIImage(systemName: "bell.slash")
+            }
+        } else {
+            UserDefaults.standard.set(1, forKey: "BellCheck") // 벨 이미지의 상태
+            alert("매일 아침 알림을 등록되었습니다.") {
+                UserDefaults.standard.set(self.Model.books[self.detailViewModel.row].gubun, forKey: "BellLocation") // 어떤 지역을 on했는가
+                self.naviRightItem.image = UIImage(systemName: "bell")
+            }
+        }
+        
+    }
+    
+    
 }
 
 extension DetailViewController : UITableViewDataSource {
@@ -45,7 +74,34 @@ extension DetailViewController : UITableViewDataSource {
     
         return detailCell
     }
+}
+
+extension DetailViewController {
     
+    func alert(_ message : String, completion : (()->Void)? = nil) {
+        
+        // 메인 스레드에서 실행하도록 변경
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+                completion?() // completion 매개변수의 값이 nil이 아닐 때에만 실행되도록
+            }
+            
+            alert.addAction(okAction)
+            self.present(alert, animated: false, completion: nil)
+        }
+    }
+    
+    
+    func naviImageSet() {
+        for _ in 0..<Model.books.count {
+            if UserDefaults.standard.string(forKey: "BellLocation") == Model.books[detailViewModel.row].gubun {
+                self.naviRightItem.image = UIImage(systemName: "bell")
+                break
+            }
+        }
+    }
 }
 
 
--- Covid19AlarmApp/ViewController/SplashViewController.swift
+++ Covid19AlarmApp/ViewController/SplashViewController.swift
@@ -25,7 +25,14 @@ class SplashViewController : UIViewController {
     
     func checkDeviceNetworkStatus() {
         
-        if Model.books.count == 0 {
+        if !(DeviceManager.shared.networkStatue) {
+            let alert: UIAlertController = UIAlertController(title: "네트워크 상태 확인", message: "네트워크가 불안정 합니다.", preferredStyle: .alert)
+            let action: UIAlertAction = UIAlertAction(title: "다시 시도", style: .default, handler: { (action) in
+                self.checkDeviceNetworkStatus()
+            })
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        } else if Model.books.count == 0 {
             let AlertDialog = UIAlertController(title: "APICALL정보를 받아올 수 없습니다.", message: "NETWORK ERROR", preferredStyle: .alert)
         
             let okAction = UIAlertAction(title: "확인", style: .default) {
@@ -40,28 +47,10 @@ class SplashViewController : UIViewController {
                 print("ERROR")
                 return
             }
-            print("CR")
+            vc.modalTransitionStyle = .crossDissolve
             vc.modalPresentationStyle = .fullScreen
             present(vc, animated: true, completion: nil)
         }
         
-//        if(DeviceManager.shared.networkStatue) {
-//            guard let vc = self.storyboard?.instantiateViewController(identifier: "firstVC") else {
-//                print("ERROR")
-//                return
-//            }
-//            print("CR")
-//            vc.modalPresentationStyle = .fullScreen
-//            present(vc, animated: true, completion: nil)
-//        } else {
-//            print("CFGE")
-//            let alert: UIAlertController = UIAlertController(title: "네트워크 상태 확인", message: "네트워크가 불안정 합니다.", preferredStyle: .alert)
-//            let action: UIAlertAction = UIAlertAction(title: "다시 시도", style: .default, handler: { (action) in
-//                self.checkDeviceNetworkStatus()
-//            })
-//            alert.addAction(action)
-//            present(alert, animated: true, completion: nil)
-//        }
     }
-    
 }
